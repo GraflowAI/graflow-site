3c9c0d451b2cb7474a8405c0950e59fb228f7bf6
Makoto Yui <myui@apache.org>
2026-02-05 00:38:14 +0900

    Update site URL to graflow.ai and add CNAME file for custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://graflowai.github.io',
+  url: 'https://graflow.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: '/',
 
